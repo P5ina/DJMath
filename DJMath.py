9581252108d5fa1a6fb3b247c7c2f5ffde2f0cfd
Timur Turatbekov <timatur242005@gmail.com>
2018-10-23 08:53:51 +0300

    init commit

--- DJMath.py
+++ DJMath.py
@@ -2,8 +2,33 @@
 import youtube_dl
 import subprocess
 import urllib
+import os
 from bs4 import BeautifulSoup
 from discord.ext import commands
+
+from discord import opus
+
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+
+
+
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(opus_libs)))
+load_opus_lib()
 
 TOKEN ='NDk4MTYzMzQyMDc5NDI2NTcw.DppuaA.uSJG9JYJXhlTHDpgEuKTsCEHja8'
 
